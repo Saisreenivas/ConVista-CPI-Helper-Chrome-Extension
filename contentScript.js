5e57abf32631b20423e4a7955947004509c925cc
Dominic Beckbauer <dominic.beckbauer@convista.com>
2020-04-15 10:05:49 +0200

    wip

--- contentScript.js
+++ contentScript.js
@@ -518,6 +518,22 @@ async function clickTrace(e) {
     return html;
   }
 
+  var getDebugGroovyContent = async function (object) {
+
+
+
+
+    var traceId = JSON.parse(await makeCallPromise("GET", "/itspaces/odata/api/v1/MessageProcessingLogRunSteps(RunId='" + object.runId + "',ChildCount=" + object.childCount + ")/TraceMessages?$format=json", true)).d.results[0].TraceId;
+
+    let properties = JSON.parse(await makeCallPromise("GET", "/itspaces/odata/api/v1/TraceMessages(" + traceId + ")/ExchangeProperties?$format=json", true)).d.results;
+    let headers = JSON.parse(await makeCallPromise("GET", "/itspaces/odata/api/v1/TraceMessages(" + traceId + ")/Properties?$format=json", true)).d.results;
+    let body = await makeCallPromise("GET", "/itspaces/odata/api/v1/TraceMessages(" + traceId + ")/$value", true);
+
+
+
+
+  }
+
   var id = this.id.replace(/BPMN[a-zA-Z-]+_/, "");
 
   var targetElements = inlineTraceElements.filter((element) => {
@@ -562,6 +578,12 @@ async function clickTrace(e) {
         childCount: childCount,
         runId: runId,
         traceType: "logContent"
+      }, {
+        label: "Debug Groovy",
+        content: getDebugGroovyContent,
+        active: false,
+        childCount: childCount,
+        runId: runId
       }
       ]
 
@@ -1597,33 +1619,42 @@ function storeVisitedIflowsForPopup() {
   });
 }
 
+
+
+function openGroovyIde(object) {
+  var confirm = getConfirmation("You are redirected to https://groovyide.com/cpi. Please notice that the ConVista CPI Helper is not responsible for any third party content.\nDo you want to proceed?")
+  if (confirm) {
+    var string = '{"input":{"body":"hallo","headers":{"oldHeader":"Hello!"},"properties":{}},"script":{"code":"def Message processData(Message message) { def body = message.getBody(); \\nmessage.setBody((body));    return message;}"}}';
+
+    function encode_utf8(s) {
+      return unescape(encodeURIComponent(s));
+    }
+
+    deflated = RawDeflate.deflate(encode_utf8(string));
+
+    data = btoa(deflated).replace(/\+/g, '-').replace(/\//g, '_');
+
+    var url = "https://groovyide.com/cpi/share/v1/" + data;
+
+    window.open(url, '_blank');
+  }
+
+
+
+
+  //accepted
+  //NY4xDsMgDEWvQj2BFPUAqbpUHbL0BllocJNIKSAgQ4S4e-1QGMD89z4iw2r9nqDP8HbmgB4WvW0OOlhQGwyRidvMcN4ID0j4AqUDH5zHkFZkp1AQp7D686nJGSTX4Ee8MEY9oyB7ovGpk5Yt-9ZTiTza0Qpa3OB_iHuD1xnTgxKpbqxUre7NiH9DclOpCgOmPdjmULeMFkr5AQ
+  //NY5BDoMgEEWvQmcliekBbLppunDTG7ihMlUTC2TARWO4uzNSWMDw3_uEHRYXtgTdDm9vf9DBbNbVQwszGosUhfjV9ueNcY-ML5BbCOQDUlpQnMxBHGkJ51Ojt8iuxY96YYxmQsX2yOPTJNPU7FtOrfbBDU7xkob8Q90rvE6YHpw0-iZK0cpejfg3GmlqXSBh2shVh7t5cJDzAQ
+
+  //Not Accepted
+  //NY4xDsMgDEV3TkE9gRT1AIm6VB2y9BAU3AQpDQjIECHuXkOLB7D_eyBnsLs_EowZXs6cMMKqts3BACsqgyFW4jYzt4nwjIQvUAbwwXkMyWJ1CgVRB-vbV9oZJNfgmz8xRrUgJ1tT-1BJiZ59frfkmTFOVf26Bb91dF0w3SkRciKjSe3oOP6xqM-kbCxgOsLelYkVBqV8AQ==
+}
+
+
 //start
 checkURLchange();
 setInterval(function () {
   checkURLchange(window.location.href);
 }, 4000);
 
-
-var string = '{"input":{"body":"hallo","headers":{"oldHeader":"Hello!"},"properties":{}},"script":{"code":"def Message processData(Message message) { def body = message.getBody(); \\nmessage.setBody((body));    return message;}"}}';
-function encode_utf8(s) {
-  return unescape(encodeURIComponent(s));
-}
-
-function decode_utf8(s) {
-  return decodeURIComponent(escape(s));
-}
-
-deflated = RawDeflate.deflate(encode_utf8(string));
-
-data = btoa(deflated);
-
-data3 = data.replace(/\+/g, '-').replace(/\//g, '_')
-
-console.log(data3);
-
-//accepted
-//NY4xDsMgDEWvQj2BFPUAqbpUHbL0BllocJNIKSAgQ4S4e-1QGMD89z4iw2r9nqDP8HbmgB4WvW0OOlhQGwyRidvMcN4ID0j4AqUDH5zHkFZkp1AQp7D686nJGSTX4Ee8MEY9oyB7ovGpk5Yt-9ZTiTza0Qpa3OB_iHuD1xnTgxKpbqxUre7NiH9DclOpCgOmPdjmULeMFkr5AQ
-//NY5BDoMgEEWvQmcliekBbLppunDTG7ihMlUTC2TARWO4uzNSWMDw3_uEHRYXtgTdDm9vf9DBbNbVQwszGosUhfjV9ueNcY-ML5BbCOQDUlpQnMxBHGkJ51Ojt8iuxY96YYxmQsX2yOPTJNPU7FtOrfbBDU7xkob8Q90rvE6YHpw0-iZK0cpejfg3GmlqXSBh2shVh7t5cJDzAQ
-
-//Not Accepted
-//NY4xDsMgDEV3TkE9gRT1AIm6VB2y9BAU3AQpDQjIECHuXkOLB7D_eyBnsLs_EowZXs6cMMKqts3BACsqgyFW4jYzt4nwjIQvUAbwwXkMyWJ1CgVRB-vbV9oZJNfgmz8xRrUgJ1tT-1BJiZ59frfkmTFOVf26Bb91dF0w3SkRciKjSe3oOP6xqM-kbCxgOsLelYkVBqV8AQ==
+openGroovyIde();
